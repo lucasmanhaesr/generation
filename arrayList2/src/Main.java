d983ac82e91652b016c38c6298b67d7b3fd57585
lucasmanhaesr <ramosmanhaes@outlook.com>
2024-12-07 13:37:53 -0300

    Exercicio ArraList 2

--- arrayList2/src/Main.java
+++ arrayList2/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        List<Integer> lista = new ArrayList<>();
+
+        Collections.addAll(lista,5,1,3,4,9,7,8,10,6);
+
+        System.out.print("Digite um número: ");
+        Integer numero = scanf.nextInt();
+
+        if(lista.contains(numero)) {
+            System.out.println("O numero " + numero + " está localizado na posição " + lista.indexOf(numero));
+        }
+        else{
+            System.out.println("O numero " + numero + " não foi localizado");
+        }
 
     }
 }
