ad599212a58fcf6e6f203fb3480380f239876e9c
lucasmanhaesr <ramosmanhaes@outlook.com>
2024-12-07 12:05:52 -0300

    Exercicio ArrayList 1

--- arrayList1/src/Main.java
+++ arrayList1/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        List<String> lista = new ArrayList<String>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite a cor: ");
+            String cor = scanf.nextLine();
+            lista.add(cor);
+        }
+
+        //Cores adicionadas
+        System.out.print("Cores adicionadas: [" + lista.get(0));
+        for(int i = 1; i < lista.size(); i++) {
+            System.out.print(", " + lista.get(i));
+        }
+        System.out.println("]");
+
+//        Cores em ordem crescente
+        Collections.sort(lista);
+        System.out.print("\nCores em ordem crescente: [" + lista.get(0));
+        for(int i = 1; i < lista.size(); i++) {
+            System.out.print(", " + lista.get(i));
+        }
+        System.out.println("]");
+
 
     }
 }
