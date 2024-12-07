1b395b7f75a1e50849368c3c2df40400c8d07e7a
lucasmanhaesr <ramosmanhaes@outlook.com>
2024-12-07 13:59:45 -0300

    Exercicio HashSet 1

---
+++ hashSet 1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        Set<Integer> lista = new HashSet<>();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.print("Digite um numero: ");
+            Integer numero = scanf.nextInt();
+            lista.add(numero);
+        }
+
+        System.out.print("\nElementos do HashSet: [ ");
+        lista.forEach(numero -> System.out.print(numero + ", "));
+        System.out.print("]");
+    }
+}
+
